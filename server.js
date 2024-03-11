03a3233f94e5d3d89c77977e7327cc0ea0860062
Deepak Lokanath <dlokanath@saltagroup.com>
2024-03-11 08:58:14 +0100

    Day 2

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express();
+
+//middlewares
+
+
+
+//routes
+
+
+
+//Error handlers
+
+
+
+//listen to server
+const PORT = process.env.PORT || 9000;
+app.listen(PORT,console.log(`Server is up and running on port ${PORT}`));
